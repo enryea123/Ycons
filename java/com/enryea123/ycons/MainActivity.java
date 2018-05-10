6b5e42e75d51307edef2dbc09c0a01632ae6371c
Enrico Albano <enryea123@users.noreply.github.com>
2018-05-10 18:06:16 +0200

    Add files via upload

---
+++ java/com/enryea123/ycons/MainActivity.java
@@ -0,0 +1,13 @@
+package com.enryea123.ycons;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
